1dbdd593760d9fcc26b1ee412aef478d6a35180c
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-22 22:56:30 +0900

    feat(member) : memberservice 추가

--- src/main/java/gb6105/inventory/coupon/service/MemberService.java
+++ src/main/java/gb6105/inventory/coupon/service/MemberService.java
@@ -1,2 +1,19 @@
-package gb6105.inventory.coupon.service;public class MemberService {
+package gb6105.inventory.coupon.service;
+
+import gb6105.inventory.coupon.domain.Member;
+import gb6105.inventory.coupon.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public void registerMember(String email) {
+        Member member = new Member(email);
+        if(!memberRepository.findByEmail(email).isPresent()) {
+            memberRepository.save(member);
+        }
+    }
 }
