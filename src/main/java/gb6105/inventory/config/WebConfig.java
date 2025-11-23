2c6345a3f736674a45197432f349bfeece62d86a
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-11-23 23:05:11 +0900

    fix(config) : 도메인 주소 추가

--- src/main/java/gb6105/inventory/config/WebConfig.java
+++ src/main/java/gb6105/inventory/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
     private static final String AWS_IP_ADDRESS = "3.38.114.206";
+    private static final String AWS_DOMAIN = "coupon.taegyunkim.com";
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -17,7 +18,9 @@ public class WebConfig {
                 registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS를 적용
                         .allowedOrigins("http://127.0.0.1:5173",
                                 "http://localhost:5173",
-                                "http://" + AWS_IP_ADDRESS + ":5173") // 허용할 출처
+                                "http://" + AWS_IP_ADDRESS + ":5173",
+                                "http://" + AWS_DOMAIN + ":5173"
+                        ) // 허용할 출처
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드
                         .allowedHeaders("*") // 모든 헤더를 허용
                         .allowCredentials(true) // 자격 증명을 허용
